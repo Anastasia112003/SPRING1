78f0876728551ce673b09615862d3fe39c605852
Anastasia112003 <113174652+Anastasia112003@users.noreply.github.com>
2023-06-09 17:17:05 +0400

    Update MainServlet.java

--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -17,7 +17,7 @@ public class MainServlet extends HttpServlet {
    @Override
     public void init() {
         final var context = new AnnotationConfigApplicationContext("ru.netology");
-        postController = context.getBean(PostController.class);
+        сontroller = context.getBean(PostController.class);
     }
 
   @Override
