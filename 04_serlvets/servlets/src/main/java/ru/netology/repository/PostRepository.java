36acb7e50f6e781d15d33c9d799d3761e0468698
Anastasya <03.nastya@mail.ru>
2023-06-09 15:40:27 +0400

    commit2

--- 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.Collection;
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
-
+@Repository
 public class PostRepository {
   ConcurrentHashMap<Long,Post> allPosts;
   AtomicLong counter = new AtomicLong();
